c82f2682bcf307588279ce9147f1b0d27506c3c5
Ramon Noleto <devnoleto@gmail.com>
2024-10-10 13:30:55 -0300

    :notebook: Criando livewire components

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app>
-    <livewire:projects.show :project="$project"/>
+    <livewire:projects.show :$project/>
 
 
-    <livewire:projects.proposals :project="$project"/>
+    <livewire:projects.proposals :$project/>
 </x-layouts.app>
