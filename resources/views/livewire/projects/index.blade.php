6841040a90df5f908c5201a2a27e66a39cc21fcc
Ramon Noleto <devnoleto@gmail.com>
2024-10-10 15:47:37 -0300

    :notebook: Ajustando Blades
    Criação de blade para status e ajuste para carregamento dinamico do estilo de status para Open e Closed, carregamento de icones das stacks de cada projeto de forma dinamica em project-card e project-card-simple

--- resources/views/livewire/projects/index.blade.php
+++ resources/views/livewire/projects/index.blade.php
@@ -1,13 +1,7 @@
-<div>
+<div class="grid grid-cols-2 gap-4 ">
     @foreach($this->projects as $project)
-
-    <li>
         <a href="{{ route('projects.show', $project) }}">
-            
-            <x-project-card :$project />
-
+            <x-project-card-simple :$project />
         </a>
-    </li>
-
     @endforeach
 </div>
