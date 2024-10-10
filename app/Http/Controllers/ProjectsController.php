795839d2e89baa7ad28d1b084fbab5af6e0eb294
Ramon Noleto <devnoleto@gmail.com>
2024-10-10 12:55:47 -0300

    :notebook: fix: Página de Projeto
    Ajuste de rota para projeto selecionado

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectsController extends Controller
 {
     public function index()
     {
-        return view('project.index');
+        return view('projects.index');
     }
 
     public function show(Project $project)
     {
         dd($project);
-
         return view('projects.show', compact('project'));
     }
+
 }
