c0c4c16614cff4bb9a3b8bb43a05513c459ec114
Ramon Noleto <ramon.zap@hotmail.com>
2024-10-12 09:50:03 -0300

    :notebook: BKP Aula
    Aula 3 em 1:16:08 Funcionando

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -33,16 +33,12 @@ class Create extends Component
             return;
         }
 
-        DB::transaction(function() {
-
-            $proposal = $this->project->proposals();
-    
-            $this->project->proposals()
+        DB::transaction(function () {
+            $proposal = $this->project->proposals()
                 ->updateOrCreate(
                     ['email' => $this->email],
                     ['hours' => $this->hours]
                 );
-
             $this->arrangePositions($proposal);
         });
 
@@ -55,14 +51,13 @@ class Create extends Component
 
     public function arrangePositions(Proposal $proposal)
     {
-        $query = DB::select("
-            select *, row_number() over (order by hours asc) as newPosition 
+        $query = DB::select('
+            select *, row_number() over (order by hours asc) as newPosition
             from proposals
             where project_id = :project
-            ", ['project'=> $proposal->project_id]);
+            ', ['project' => $proposal->project_id]);
         $position = collect($query)->where('id', '=', $proposal->id)->first();
-        $otherProposal = cpççect($query)->where('position', '=', $position->newPosition)->first();
-
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
         if($otherProposal) {
             $proposal->update(['position_status' => 'up']);
             Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
